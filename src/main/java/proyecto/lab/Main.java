53eec8132edde3ac60955e018bf578070bdfcba2
dani-pp <pesodaniza@gmail.com>
2025-10-11 22:13:56 -0300

    correciones menores service

--- src/main/java/proyecto/lab/Main.java
+++ src/main/java/proyecto/lab/Main.java
@@ -2,6 +2,7 @@ package proyecto.lab;
 
 import proyecto.lab.server.dao.UsuarioDAO;
 import proyecto.lab.server.models.Usuario;
+import proyecto.lab.server.service.UsuarioService;
 
 import java.sql.SQLException;
 
@@ -10,27 +11,13 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         //Conexion conn = new Conexion();
+        UsuarioService usuarioservice = new UsuarioService();
+        Usuario u = new Usuario(1,"lagosh","habilitado","danilago123");
 
-        int ID = 14;
-        String nombre = "carlos";
-        String estado = "activo";
-        String password = "1234";
-
-        //crear un objeto con datos (totalmente aislado de la logica de la base de datos)
-        Usuario user = new Usuario(ID, nombre, estado, password);
-
-        //ingresar el objeto creado anteriormente a DAO, para poder ingresarlo a la BD
-        UsuarioDAO dao = new UsuarioDAO();
-        Usuario u = dao.buscarUsuario(2, "vito");
-        System.out.println(u.getNombre());
-        System.out.println(u.getID());
-        System.out.println(u.getPassword());
+//        usuarioservice.crearUsuario(u);
+        usuarioservice.actualizarNombreUsuario(u,"dani");
+//        usuarioservice.habilitarUsuario(u);
 
 
-        if(dao.ActualizarUsuario(u, 1, "coni")){
-            /* forma 1  u = dao.buscarUsuario(2, "alonso"); */
-            /* forma 2 u.setNombre("coni");*/
-            System.out.println(u.getNombre());
-        }
     }
 }
