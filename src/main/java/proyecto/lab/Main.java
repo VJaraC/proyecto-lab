e952204ce6027fff163fd2ce2cab7b37e6ffac44
Vito <vitoimportante2@gmail.com>
2025-10-20 12:24:12 -0300

    Parche 20-10

--- src/main/java/proyecto/lab/Main.java
+++ src/main/java/proyecto/lab/Main.java
@@ -3,6 +3,7 @@ package proyecto.lab;
 import proyecto.lab.server.controller.AdminController;
 import proyecto.lab.server.dao.UsuarioDAO;
 import proyecto.lab.server.dto.UsuarioDTO;
+import proyecto.lab.server.dto.UsuarioLoginDTO;
 import proyecto.lab.server.dto.UsuarioUpdateDTO;
 import proyecto.lab.server.models.Usuario;
 import proyecto.lab.server.service.UsuarioService;
@@ -13,22 +14,16 @@ import java.sql.SQLException;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws SQLException {
-        try{
+
         UsuarioDAO usuarioDAO = new UsuarioDAO();
         UsuarioService usuarioService = new UsuarioService(usuarioDAO);
         AdminController adminController = new AdminController(usuarioService);
 
-//        UsuarioLoginDTO nuevoUsuario = new UsuarioLoginDTO("danilagosh","12345");
-
-//        UsuarioDTO resultado = adminController.crearUsuario(nuevoUsuario);
-
-         for(UsuarioDTO usuario : usuarioService.listarUsuarios()){
-             System.out.println("ID : " + usuario.getID() + " Nombre : " + usuario.getNombre()
-              + "Estado : " + usuario.getEstado());
-         }
-        }catch(Exception e){
-            System.out.println("Error al crear usuario: " + e.getMessage());
-        }
+        UsuarioLoginDTO nuevousuario = new UsuarioLoginDTO();
+        nuevousuario.setNombre("Vito");
+        nuevousuario.setRut("21243169-9");
+        nuevousuario.setContrasena("vito123");
+        adminController.crearUsuario(nuevousuario);
 
     }
 }
