4819dfe83d1c3b7c8a71a886084c42c98f757056
Vito <vitoimportante2@gmail.com>
2025-10-28 16:52:19 -0300

    Parche 28-10

--- src/main/java/proyecto/lab/Main.java
+++ src/main/java/proyecto/lab/Main.java
@@ -2,10 +2,13 @@ package proyecto.lab;
 
 import proyecto.lab.server.controller.UsuarioController;
 import proyecto.lab.server.dao.UsuarioDAO;
+import proyecto.lab.server.dto.UsuarioDTO;
 import proyecto.lab.server.dto.UsuarioLoginDTO;
+import proyecto.lab.server.models.Rol;
 import proyecto.lab.server.service.UsuarioService;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -18,9 +21,23 @@ public class Main {
 
         UsuarioLoginDTO nuevousuario = new UsuarioLoginDTO();
         nuevousuario.setNombre("Vito");
+        nuevousuario.setApellidos("Jara");
         nuevousuario.setRut("21243169-9");
         nuevousuario.setContrasena("vito123");
-        usuarioController.crearUsuario(nuevousuario);
+        nuevousuario.setGenero("Masculino");
+        nuevousuario.setEmail("nuevousuario@example.com");
+        nuevousuario.setCargo("TI");
+        nuevousuario.setTelefono("964031692");
+        nuevousuario.setFecha_nacimiento(LocalDate.of(2002,5,12));
+
+
+        UsuarioDTO adminAuth = new UsuarioDTO();
+        adminAuth.setID(777);
+        adminAuth.setNombre("Administrador");
+        adminAuth.setRol(Rol.ADMIN);
+
+        usuarioController.crearUsuario(nuevousuario,adminAuth);
+
 
     }
 }
