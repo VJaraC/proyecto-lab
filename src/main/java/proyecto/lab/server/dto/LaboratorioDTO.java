59f8fbd2ec13907938d37e633e8c23d4085581ef
alhidalgog <alonso.hidalgo004@gmail.com>
2025-11-03 19:08:16 -0300

    ajustes menores

--- src/main/java/proyecto/lab/server/dto/LaboratorioDTO.java
+++ src/main/java/proyecto/lab/server/dto/LaboratorioDTO.java
@@ -4,19 +4,6 @@ import java.time.LocalDate;
 
 public record LaboratorioDTO(Integer id_lab, String nombre_lab, String ubicacion, int capacidad_personas, int capacidad_equipo, String estado_lab, LocalDate fecha_registro_lab){
 
-    //constructor
-    public LaboratorioDTO(Laboratorio laboratorio) {
-        this(
-                laboratorio.getId_lab(),
-                laboratorio.getNombre_lab(),
-                laboratorio.getUbicacion(),
-                laboratorio.getCapacidad_personas(),
-                laboratorio.getCapacidad_equipo(),
-                laboratorio.getEstado_lab(),
-                laboratorio.getFecha_registro_lab()
-        );
-    }
-
     public LaboratorioDTO( String nombre_lab, String ubicacion, int capacidad_personas, int capacidad_equipo, LocalDate fecha_registro_lab) {
             this(null, nombre_lab,ubicacion,capacidad_personas,capacidad_equipo,null,fecha_registro_lab);
     }
